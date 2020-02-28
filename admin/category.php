92490ed6af4325c45ff1b72118666beb58bb7c24
unknown <riponcmt630371@gmail.com>
2020-02-28 16:17:45 +0600

    Read php code ok

--- admin/category.php
+++ admin/category.php
@@ -32,7 +32,7 @@
                 <!--all category start-->
                 <div class="card card-primary collapsed-card">
                 <div class="card-header">
-                    <h3 class="card-title">Expandable</h3>
+                    <h3 class="card-title">ALL Catagory </h3>
 
                     <div class="card-tools">
                     <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-plus"></i>
@@ -41,10 +41,39 @@
                     <!-- /.card-tools -->
                 </div>
                 <!-- /.card-header -->
-                <div class="card-body" style="display: none;">
-                    The body of the card
-                </div>
-                <!-- /.card-body -->
+                    <div class="card-body" style="display: none;">
+                        <!--catagory table start-->
+                        <table class="table">
+                            <thead class="thead-dark">
+                                <tr>
+                                <th scope="col">Sl.</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Description</th>
+                                <th scope="col">Action</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                            <?php
+                                $query = "SELECT * FROM `category`";
+                                $all_cat = mysqli_query($db, $query);
+                                while($row = mysqli_fetch_assoc($all_cat)){
+                                    $cat_id = $row['id'];
+                                    $cat_name = $row['name'];
+                                    $cat_desc = $row['description'];
+
+                                }?>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td><?php echo $cat_name; ?></td>
+                                    <td><?php echo  $cat_desc; ?></td>
+                                    <td>@mdo</td>
+                                </tr>
+                            <?php ?>
+                            </tbody>
+                        </table>
+                        <!--category table end -->
+                    </div>
+                    <!-- /.card-body -->
                 </div>
                 <!-- all category end -->
             </div>
